b50edcc95c8081725c425ee27a930e691ccc953d
seunghun yang <yabby1997@gmail.com>
2021-03-18 16:39:22 +0900

    45. 5. Let’s create a vertical grid layout for the products

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -10,33 +10,7 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
     
-    // MARK: - BODY
-    var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                NavigationBarView()
-                    .padding(.horizontal, 15)
-                    .padding(.bottom)
-                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
-                    .background(Color.white)
-                    .shadow(color: Color.black.opacity(0.05), radius: 10, x: 0, y: 5)
-                
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(spacing: 0) {
-                        FeaturedTabView()
-                            .padding(.vertical, 20)
-                        
-                        CategoryGridView()
-                    } //: VStack
-                } //: ScrollView
-                
-                FooterView()
-                    .padding(.horizontal)
-            }
-            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
-        } //: ZStack
-        .ignoresSafeArea(.all, edges: .top)
-    }
+    x
 }
 
 // MARK: - PREVIEW
---
+++ Touchdown/View/Home/ProductItemView.swift
@@ -0,0 +1,45 @@
+//
+//  ProductItemView.swift
+//  Touchdown
+//
+//  Created by Seunghun Yang on 2021/03/18.
+//
+
+import SwiftUI
+
+struct ProductItemView: View {
+    // MARK: - PROPERTIES
+    let product: Product
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            ZStack {
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(15)
+            }
+            .background(Color(red: product.red, green: product.green, blue: product.blue))
+            .cornerRadius(12)
+            
+            Text(product.name)
+                .font(.title3)
+                .fontWeight(.black)
+            
+            Text(product.formattedPrice)
+                .fontWeight(.semibold)
+                .foregroundColor(.gray)
+        } //: VStack
+    }
+}
+
+// MARK: - PREVIEW
+struct ProductItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductItemView(product: products[0])
+            .previewLayout(.fixed(width: 200, height: 300))
+            .padding()
+            .background(colorBackground)
+    }
+}
