dd96795f307b49427eae1ac0e8e85a3ea23669d9
seunghun yang <yabby1997@gmail.com>
2021-03-18 12:20:00 +0900

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Seunghun Yang on 2021/03/18.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
