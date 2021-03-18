2277318ead507f751370161e82302c346af63d16
seunghun yang <yabby1997@gmail.com>
2021-03-18 12:59:16 +0900

    41. 1. Let’s set up the Touchdown iOS 14 project in Xcode 12

---
+++ Touchdown/Utility/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  Touchdown
+//
+//  Created by Seunghun Yang on 2021/03/18.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+// UX
+
+// API
+
+// IMAGE
+
+// FONT
+
+// STRING
+
+// MISC
+
---
+++ Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,44 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by Seunghun Yang on 2021/03/18.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("We offer the most cutting edge, comfortable, lightweight and durable footbal helmets in the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright © Seunghun Yang\nAll right reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        } //: VStack
+        .padding()
+    }
+}
+
+// MARK: - PREVIEW
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
