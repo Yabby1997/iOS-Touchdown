deab3b6c0facae87521cb80b85939c7963615f9f
seunghun yang <yabby1997@gmail.com>
2021-03-18 13:46:29 +0900

    42. 2. Let’s create a custom navigation bar with a logo

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,41 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Seunghun Yang on 2021/03/18.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } //: HStack
+    }
+}
+
+// MARK: - PREVIEW
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
---
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,59 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Seunghun Yang on 2021/03/18.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    // MARK: - PROPERTIES
+    @State private var isAnimated: Bool = false
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Button(action: {}) {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            } //: Magnifier Button
+            
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
+            
+            Spacer()
+            
+            Button(action: {}) {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            } //: Cart Button
+        } //: HStack
+    }
+}
+
+// MARK: - PREVIEW
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
